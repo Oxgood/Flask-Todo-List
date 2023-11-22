5875cf20addf772487e8a99ca4f027d0e322cf1d
Oxgood Atta-Agyekum <agyekumoxgood@gmail.com>
2023-11-22 17:40:30 +0000

    Closes #1: Issue number1 solved

--- app.py
+++ app.py
@@ -12,21 +12,22 @@ lists = []
 @app.route("/", methods=['POST', 'GET'])
 def home():
     form = TodoForms()
-    if form.validate_on_submit():
-        todo_name = form.todo_name.data
-        todo_date = form.todo_date.data
-        todo_obj = {
-            'todo_name': todo_name,
-            'todo_date': todo_date
-        }
-        lists.append(todo_obj)
-        form.todo_name.data = None
-        form.todo_date.data = None
-        return redirect(url_for('home'))
-    elif request.form:
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            todo_name = form.todo_name.data
+            todo_date = form.todo_date.data
+            todo_obj = {
+                'todo_name': todo_name,
+                'todo_date': todo_date
+            }
+            lists.append(todo_obj)
+            form.todo_name.data = None
+            form.todo_date.data = None
+            return redirect(url_for('home'))
         todo_index = int(request.form.get('todo_index'))
-        del lists[todo_index]
-        return redirect(url_for('home'))
+        if 0 <= todo_index < len(lists):
+            del lists[todo_index]
+            return redirect(url_for('home'))
     return render_template('TodoList.html', form=form, lists=lists)
 
 
